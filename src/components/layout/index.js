9adef8e3b6ac48124ec8b54837f46fbd6def3774
joe-delaney <joseph.delaney546@gmail.com>
2022-05-13 22:09:56 -0400

    starting portfolio site

---
+++ src/components/layout/index.js
@@ -0,0 +1,7 @@
+import "./index.scss";
+
+const Layout = () => {
+    return <>Test</>
+}
+
+export default Layout;
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,7 @@
+import "./index.scss";
+
+const Sidebar = () => {
+
+}
+
+export default Sidebar;
